d7e1daa489bf389a198ef087bdc4a3bec6cb3028
Andrey Trubin <trubin23@gmail.com>
2018-02-19 18:44:32 +0500

    include libraries in gradle and change layout tasks_act

--- app/src/main/java/ru/trubin23/tasks_mvp_dagger/TasksActivity.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_dagger/TasksActivity.java
@@ -1,15 +1,14 @@
 package ru.trubin23.tasks_mvp_dagger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class MainActivity extends DaggerAppCompatActivity {
+public class TasksActivity extends DaggerAppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tasks_act);
     }
 }
