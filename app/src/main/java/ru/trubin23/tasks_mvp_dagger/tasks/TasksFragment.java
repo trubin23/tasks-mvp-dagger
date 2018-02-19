9f8018990a6ffc97810261320a3eedfa0c4d0a2b
Andrey Trubin <trubin23@gmail.com>
2018-02-19 23:58:36 +0500

    start adding classes for interaction TasksActivity

---
+++ app/src/main/java/ru/trubin23/tasks_mvp_dagger/tasks/TasksFragment.java
@@ -0,0 +1,10 @@
+package ru.trubin23.tasks_mvp_dagger.tasks;
+
+import dagger.android.support.DaggerFragment;
+
+/**
+ * Created by Andrey on 19.02.2018.
+ */
+
+public class TasksFragment extends DaggerFragment implements TasksContract.View {
+}
---
+++ app/src/main/java/ru/trubin23/tasks_mvp_dagger/tasks/TasksPresenter.java
@@ -0,0 +1,9 @@
+package ru.trubin23.tasks_mvp_dagger.tasks;
+
+/**
+ * Created by Andrey on 19.02.2018.
+ */
+
+public class TasksPresenter implements TasksContract.Presenter {
+
+}
