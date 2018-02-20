a61aeaa17f0bf64b36bc72572f5d061abcebad1a
Andrey Trubin <trubin23@gmail.com>
2018-02-20 19:14:43 +0500

    start implementation TasksRepository

--- app/src/main/java/ru/trubin23/tasks_mvp_dagger/data/source/TasksRepository.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_dagger/data/source/TasksRepository.java
@@ -1,8 +1,21 @@
 package ru.trubin23.tasks_mvp_dagger.data.source;
 
+import javax.inject.Singleton;
+
 /**
  * Created by Andrey on 20.02.2018.
  */
 
+@Singleton
 public class TasksRepository implements TasksDataSource {
+
+    private final TasksDataSource mTaskRemoteDataSource;
+    private final TasksDataSource mTaskLocalDataSource;
+
+
+    public TasksRepository(@Remote TasksDataSource taskRemoteDataSource,
+                           @Local TasksDataSource taskLocalDataSource) {
+        mTaskRemoteDataSource = taskRemoteDataSource;
+        mTaskLocalDataSource = taskLocalDataSource;
+    }
 }
---
+++ app/src/main/java/ru/trubin23/tasks_mvp_dagger/data/source/local/TasksLocalDataSource.java
@@ -0,0 +1,10 @@
+package ru.trubin23.tasks_mvp_dagger.data.source.local;
+
+import ru.trubin23.tasks_mvp_dagger.data.source.TasksDataSource;
+
+/**
+ * Created by Andrey on 20.02.2018.
+ */
+
+public interface TasksLocalDataSource extends TasksDataSource {
+}
