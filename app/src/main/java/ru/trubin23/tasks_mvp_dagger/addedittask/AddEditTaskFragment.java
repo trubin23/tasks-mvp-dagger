5dc6a282cb56d4dc4d9dc17e3ea2b105801717be
Andrey Trubin <trubin23@gmail.com>
2018-02-26 21:57:27 +0500

    implementation of layouts for the AddEditTask package

--- app/src/main/java/ru/trubin23/tasks_mvp_dagger/addedittask/AddEditTaskFragment.java
+++ app/src/main/java/ru/trubin23/tasks_mvp_dagger/addedittask/AddEditTaskFragment.java
@@ -1,8 +1,18 @@
 package ru.trubin23.tasks_mvp_dagger.addedittask;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import dagger.android.support.DaggerFragment;
+import ru.trubin23.tasks_mvp_dagger.R;
 import ru.trubin23.tasks_mvp_dagger.di.ActivityScoped;
 
 /**
@@ -15,11 +25,27 @@ public class AddEditTaskFragment extends DaggerFragment
 
     public static final String ARGUMENT_EDIT_TASK_ID = "EDIT_TASK_ID";
 
+    @BindView(R.id.add_task_title)
+    EditText mTitle;
+
+    @BindView(R.id.add_task_description)
+    EditText mDescription;
+
     @Inject
     AddEditTaskContract.Presenter mPresenter;
 
     @Inject
     public AddEditTaskFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.addtask_frag, container, false);
+        ButterKnife.bind(this, root);
+
+        return root;
     }
 
     @Override
